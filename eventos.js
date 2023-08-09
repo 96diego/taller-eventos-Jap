3e87f40ef1b6a35db0578be6ba7eced573b24252
96Diego <96DiegoTorres@gmail.com>
2023-08-09 04:15:30 -0300

    evento con solucion

--- eventos.js
+++ eventos.js
@@ -1,11 +1,18 @@
+let noSaludo=true;
 function saludar()
 {
     alert("Hola!");
+    noSaludo=false;
 };
 
 const divSaludo = document.querySelector("div")
 divSaludo.addEventListener("click",()=>
     {
+        if(noSaludo)
+        {
         alert("Hola! Soy el div");
+        noSaludo=false;
+        };
     }
 );
+noSaludo=true;
